768d532ed20a90a83da7cdb481c4ea405e5339da
M Fakhri Musyaffa <92082985+MFakhriMusyaffa@users.noreply.github.com>
2022-03-22 21:31:39 +0700

    Add files via upload

---
+++ praktikum-php-4/persegi_panjang.php
@@ -0,0 +1,27 @@
+<?php 
+class persegiPanjang{
+    public $panjang;
+    public $lebar;
+
+    function luas_pp(){
+        return $this->panjang*$this->lebar;
+    }
+    function keliling_pp(){
+        return 2*($this->panjang+$this->lebar);
+    }
+
+    function setLebar($lebar){
+        return $this->lebar=$lebar;
+    }
+    function setPanjang($panjang){
+        return $this->panjang=$panjang;
+    }
+}
+$PersegiPanjang = new persegiPanjang();
+echo "Luas dan Keliling Persegi Panjang";
+echo "<br/>";
+echo "<br/>panjang :".$PersegiPanjang -> setPanjang(10);
+echo "<br/>lebar :".$PersegiPanjang -> setLebar(10);
+echo "<br/>luas persegi panjang : ".$PersegiPanjang -> luas_pp();
+echo "<br/>Keliling persegi panjang : ".$PersegiPanjang -> keliling_pp();
+?>
